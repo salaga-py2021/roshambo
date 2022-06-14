ab865e884323693a98c6ad6d0265958c80d6e691
salaga-py2021 <pxg200018@utdallas.edu>
2022-06-14 17:16:44 -0500

    Finish game logic

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 function computerPlay(){
     let randomINT = randomInteger(1,3);
-    return (randomINT==1) ? "Rock" : (randomINT == 2) ? "Paper" : "Scissor"
+    return (randomINT==1) ? "Rock" : (randomINT == 2) ? "Paper" : "Scissor";
 }
 
 function randomInteger(min,max) {
@@ -16,19 +16,40 @@ function playRound(playerSelection,computerSelection){
     playerSelection = properCase(playerSelection);
 
     if (playerSelection == computerSelection) {
-        return `It's a tie. You both chose ${playerSelection}`
+        return `It's a tie. You both chose ${playerSelection}`;
     }
     else if ((playerSelection == "Rock" && computerSelection == "Scissor") || 
              (playerSelection == "Paper" && computerSelection == "Rock") ||
              (playerSelection == "Scissor" && computerSelection == "Paper")) {
         
-        return `You win! ${playerSelection} beats ${computerSelection}`
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
     else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
-for (let i = 0; i<=100; i++){
-    console.log(playRound(computerPlay(),computerPlay()))
+function game(){
+    let playerCount = 0, computerCount = 0; 
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose Rock, Paper, or Scissor");
+        let computerSelection = computerPlay();
+        let playRoundResult = playRound(playerSelection,computerSelection);
+
+        // Print Round Result
+        console.log(playRoundResult);
+
+        // Score Round
+        if (playRoundResult.charAt(4) == "W") playerCount++;
+        else if (playRoundResult.charAt(4) == "L") computerCount++;
+
+        // Print New Score
+        console.log(`Player Score: ${playerCount}`);
+        console.log(`Computer Score: ${computerCount}`);
+    }
+
+    let finalMessage = (playerCount == computerCount) ? "The game is a tie" : 
+                       (playerCount > computerCount) ? "You win the game!" : "You lose the game"
+
+    console.log(finalMessage)
 }
