dce16fc4cce32d08da6e47156880e4c941111bce
salaga-py2021 <pxg200018@utdallas.edu>
2022-06-14 16:26:21 -0500

    Finish playRound function
    The functoin checks the result of a single game of roshambo.

--- logic.js
+++ logic.js
@@ -1,8 +1,34 @@
 function computerPlay(){
     let randomINT = randomInteger(1,3);
-    return (randomINT==1) ? "Rock" : (randomINT == 2) ? "Paper" : "Scissors"
+    return (randomINT==1) ? "Rock" : (randomINT == 2) ? "Paper" : "Scissor"
 }
 
 function randomInteger(min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function properCase(word){
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection,computerSelection){
+    // Remove problem with case sensitivity.
+    playerSelection = properCase(playerSelection);
+
+    if (playerSelection == computerSelection) {
+        return `It's a tie. You both chose ${playerSelection}`
+    }
+    else if ((playerSelection == "Rock" && computerSelection == "Scissor") || 
+             (playerSelection == "Paper" && computerSelection == "Rock") ||
+             (playerSelection == "Scissor" && computerSelection == "Paper")) {
+        
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    }
+    else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+for (let i = 0; i<=100; i++){
+    console.log(playRound(computerPlay(),computerPlay()))
 }
